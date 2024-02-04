a5e787ea6e52a9f5e6d10e9a16fa6fc6c452c47b
imkh817 <rjs7180@naver.com>
2024-02-04 16:28:25 +0900

    서블릿 예외 처리 - 인터셉터,ExceptionResolver(1),(2)

--- src/main/java/home/exception/WebConfig.java
+++ src/main/java/home/exception/WebConfig.java
@@ -3,6 +3,7 @@ package home.exception;
 import home.exception.filter.LogFilter;
 import home.exception.interceptor.LogInterceptor;
 import home.exception.resolver.MyHandlerExceptionResolver;
+import home.exception.resolver.UserHandlerExceptionResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
         resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
     }
 
     @Override
