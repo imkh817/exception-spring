eabd5e9a099debc0577abeab7bc33d0a99d5d486
imkh817 <rjs7180@naver.com>
2024-02-05 13:28:42 +0900

    API 예외처리, ExceptionResolver(1),(2), 스프링이 제공하는 ExceptionResolver(1)

--- src/main/java/home/exception/api/ApiExceptionController.java
+++ src/main/java/home/exception/api/ApiExceptionController.java
@@ -1,12 +1,16 @@
 package home.exception.api;
 
+import home.exception.exception.BadRequestException;
 import home.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -28,6 +32,30 @@ public class ApiExceptionController {
         return new MemberDTO(id,"hello"+id);
     }
 
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2(){
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"error.bad",new IllegalArgumentException());
+        /**
+         * ResponseStatusException(HttpStatus,@Nullable String reason,@Nullable Throwable cause);
+         *
+         * HttpStatus status(필수):
+         * ResponseStatusException이 던져질 때 클라이언트에게 반환될 HTTP 응답의 상태 코드를 나타낸다.
+         * 예를 들어, HttpStatus.BAD_REQUEST 또는 HttpStatus.NOT_FOUND와 같은 상태 코드를 설정할 수 있다.
+         *
+         * String reason(선택):
+         * 클라이언트에게 반환될 오류 메시지의 이유를 나타낸다.
+         * 이는 클라이언트가 이해할 수 있는 메시지로, 예외에 대한 자세한 설명을 제공하는 데 사용된다.
+         *
+         * Throwable cause(선택):
+         * 예외의 원인이 되는 다른 예외를 나타낸다.
+         * 예외의 원인을 전달하여 디버깅 및 로깅을 용이하게 할 수 있다.
+         */
+    }
     @Data
     @AllArgsConstructor
     static class MemberDTO{
